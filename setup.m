a5c2bfcc5b2bdcd1b4bcc6280f9cd384bd5fc6c5
Jonathan <wildejono@gmail.com>
2020-05-23 00:54:40 +1000

    Formatting Update

--- setup.m
+++ setup.m
@@ -20,11 +20,12 @@ hold on
 workspace = [-2 3 -2 2 0 2]
 
 L1 = Link('d',0.700,'a',0,'alpha',pi/2,'offset',pi/2,'qlim',deg2rad([-360 360]));
-            L2 = Link('d',-0.350,'a',0,'alpha',0,'qlim',deg2rad([-180 180]));
-            L3 = Link('d',0.165,'a',0,'alpha',-pi/2,'qlim',deg2rad([-180 180]));
-            L4 = Link('d',0.350,'a',0,'alpha',pi/2,'qlim',deg2rad([-180 180]));
-            L5 = Link('d',0.097,'a',0,'alpha',-pi/2,'qlim',deg2rad([-180 180]));
-            L6 = Link('d',0.023,'a',0,'alpha',0,'qlim',deg2rad([-180 180]));
+L2 = Link('d',-0.350,'a',0,'alpha',0,'qlim',deg2rad([-180 180]));
+L3 = Link('d',0.165,'a',0,'alpha',-pi/2,'qlim',deg2rad([-180 180]));
+L4 = Link('d',0.350,'a',0,'alpha',pi/2,'qlim',deg2rad([-180 180]));
+L5 = Link('d',0.097,'a',0,'alpha',-pi/2,'qlim',deg2rad([-180 180]));
+L6 = Link('d',0.023,'a',0,'alpha',0,'qlim',deg2rad([-180 180]));
+
 model = SerialLink([L1 L2 L3 L4 L5 L6], 'name', 'robot');
 % plot(model, [0,0,0,0,0,0], 'workspace', workspace);
 model.teach();
