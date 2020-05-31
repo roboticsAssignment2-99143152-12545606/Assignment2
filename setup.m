4f325f11a26014e6c6a3f3dcbc3126831cf0f222
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-05-31 23:55:05 +1000

    mutiple shaking motions

--- setup.m
+++ setup.m
@@ -8,7 +8,7 @@ set(0,'DefaultFigureWindowStyle','docked')
 clear
 clc
 % Mac run command
-run /Users/jonathanwilde/git/Robotics/rvctools/startup_rvc.m
+% run /Users/jonathanwilde/git/Robotics/rvctools/startup_rvc.m
 % Windows run command
 %run C:\Git\Robotics\rvctools\startup_rvc.m
 
@@ -19,22 +19,21 @@ hold on
 % workspace = [-workSize workSize -workSize workSize (2*0) workSize];
 workspace = [-2 3 -2 2 0 2]
 
-% L1 = Link('d',0.700,'a',0,'alpha',pi/2,'offset',pi/2,'qlim',deg2rad([-360 360]));
-% L2 = Link('d',-0.350,'a',0,'alpha',0,'qlim',deg2rad([-180 180]));
-% L3 = Link('d',0.165,'a',0,'alpha',-pi/2,'qlim',deg2rad([-180 180]));
-% L4 = Link('d',0.350,'a',0,'alpha',pi/2,'qlim',deg2rad([-180 180]));
-% L5 = Link('d',0.097,'a',0,'alpha',-pi/2,'qlim',deg2rad([-180 180]));
-% L6 = Link('d',0.023,'a',0,'alpha',0,'qlim',deg2rad([-180 180]));
-
 % model = SerialLink([L1 L2 L3 L4 L5 L6], 'name', 'robot');
 % plot(model, [0,0,0,0,0,0], 'workspace', workspace);
 % model.teach();
 % axis equal;
 % view(3)
 % pause();
-N6_1 = D6Model('N6_1',workspace, transl(0,0,0), 1);
+N6_1 = D6Model('N6_1',workspace, transl(0,0,0));
 
-N6_1.model.teach();
+% N6_1.model.teach();
+q = deg2rad([0,50,50,0,0,0])
+N6_1.model.animate(q);
+MoveWObjects(N6_1, transl(0.3,0.3,0.1),[]);
+movementShake(N6_1, [], 1, [])
+
+'done'
 
 end
 
