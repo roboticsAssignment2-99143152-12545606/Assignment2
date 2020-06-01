bfedfa3afd90699185fd2b422045b2fed6c5ccd8
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-06-01 16:35:07 +1000

    adding function that moves many objects based on a qmatrix

---
+++ MoveQMatrix.m
@@ -0,0 +1,19 @@
+function [] = MoveQMatrix(Robot_Arm,qMatrix,Objects)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+for qStep = 1:size(qMatrix,1)
+    q = qMatrix(qStep,:);
+    Robot_Arm.model.animate(q);
+    if isempty(Objects) == 0
+        newBase = Robot_Arm.model.fkine(q);
+    end
+    for i = 1:size(Objects,2)
+        Objects(i).model.base = newBase * trotx(Objects(i).rot);
+        Objects(i).model.animate(0);
+    end
+    pause(0.01);
+end
+
+end
+
