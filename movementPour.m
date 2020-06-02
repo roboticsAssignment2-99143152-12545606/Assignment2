5ceb3f4b4b1c02d3da18e93ab187096bec8b2801
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-06-02 14:53:42 +1000

    adding step size to control functions

--- movementPour.m
+++ movementPour.m
@@ -1,14 +1,14 @@
-function [] = movementPour(Robot, GoalPose, Time, Objects)
+function [] = movementPour(Robot, GoalPose, Time, Objects, stepSize)
 %movementPour this function will simulate shaking for the robot
 %   code mainly used from lab9
 p1 = Robot.getPose;
 iterations = 1
 % movementCircle(Robot, GoalPose, Time, Objects)
-
+motions = 0;
 for i=1:iterations
-    movementCircle(Robot, p1, Time/iterations, Objects, 3, 0.3);
+    movementCircle(Robot, p1, Time/(iterations+motions), Objects, 3, 0.3, stepSize);
     %     movementCircle(Robot, p1, Time/iterations, Objects, -3, 0.3);
-    movementLine(Robot, transl(p1(1,4),p1(2,4)+0.2,p1(3,4)+0.2), Time/2, Objects);
+    movementLine(Robot, transl(p1(1,4),p1(2,4)+0.2,p1(3,4)+0.2), Time/(iterations+motions), Objects, stepSize);
 end
 
 end
