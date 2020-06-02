34d28225e739102cabdc81eb1e280283c5623ae8
Jonathan <43630244+wildejono@users.noreply.github.com>
2020-06-02 14:33:17 +1000

    Imported Models
    Established a basic environment for the robots. Van, bottle and robots have been placed.

--- setup.m
+++ setup.m
@@ -10,33 +10,35 @@ clc
 % Mac run command
 % run /Users/jonathanwilde/git/Robotics/rvctools/startup_rvc.m
 % Windows run command
-%run C:\Git\Robotics\rvctools\startup_rvc.m
+run C:\Git\Robotics\rvctools\startup_rvc.m
 
 %%
 hold on
 % floorOffset = -0.8905/2; %messured from bounding box
 % workSize = 10;
 % workspace = [-workSize workSize -workSize workSize (2*0) workSize];
-workspace = [-2 3 -2 2 0 2]
-
-% setting up  models
-N6_1 = D6Model('N6_1',workspace, transl(0,0,0));
-N6_1.model.delay = 0.1;
+workspace = [-3 3 -5 5 0 5]
 
 % setting up objects
-bottle = Objects('WildTurkey','1',workspace, transl(1,0,0.35), -pi/2);
-bottle.model.delay = 0;
+van = Objects('Van', '1', workspace, transl(0,0,0), 0);
+wildT = Objects('WildTurkey','2',workspace, transl(1,-1.75,1.87), -pi/2);
+
+van.model.delay = 0;
+wildT.model.delay = 0;
+
+% setting up  models
+N6_1 = D6Model('N6_1',workspace, transl(0,-0.5,0.600));
+N6_2 = D6Model('N6_2',workspace, transl(0,-2,0.600));
+N6_1.model.delay = 0.1;
+N6_2.model.delay = 0.1;
 
 % N6_1.model.teach();
 q = deg2rad([90,90,90,0,0,0])
 N6_1.model.animate(q);
-MoveWObjects(N6_1, transl(-0.33,0,0.4) * troty(90),[]);
+N6_2.model.animate(q);
 % N6_1.model.teach
 % 
 % pause(0.1);
-movementPour(N6_1, [], 3, [bottle])
-
-'done'
 
 end
 
