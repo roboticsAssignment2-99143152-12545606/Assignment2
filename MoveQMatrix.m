497f20cef3fce166a938e0babbaf2cac3f71e5fd
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-06-02 14:53:00 +1000

    adding ability to have setup size if defined

--- MoveQMatrix.m
+++ MoveQMatrix.m
@@ -1,8 +1,12 @@
-function [] = MoveQMatrix(Robot_Arm,qMatrix,Objects)
+function [] = MoveQMatrix(Robot_Arm,qMatrix,Objects,stepsize)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-for qStep = 1:size(qMatrix,1)
+if ~exist('stepsize')
+    stepsize = 1;
+end
+
+for qStep = 1:stepsize:size(qMatrix,1)
     q = qMatrix(qStep,:);
     Robot_Arm.model.animate(q);
     if isempty(Objects) == 0
