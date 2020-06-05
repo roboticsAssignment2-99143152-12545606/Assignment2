091523fc8605a45e5501a116441bb984bc5d6109
Jonathan <43630244+wildejono@users.noreply.github.com>
2020-06-05 13:56:51 +1000

    Add Rick + Update MoveQMatrix
    - Added Rick for collision testing
    - Updated QMatrix to WAIT for collision to move before completing the move

--- MoveQMatrix.m
+++ MoveQMatrix.m
@@ -1,6 +1,7 @@
 function [] = MoveQMatrix(Robot_Arm,qMatrix,Objects,Environment,stepsize)
 %UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%   This function will now pause movement until the obstacle has been
+%   moved
 
 if ~exist('stepsize')
     stepsize = 1;
@@ -8,25 +9,30 @@ end
 
 for qStep = 1:stepsize:size(qMatrix,1)
     q = qMatrix(qStep,:);
-    
-    for i = 1:size(Environment,2)
-        colResult = IsCollision(Robot_Arm.model,qMatrix,Environment(i).model.faces,...
-            Environment(i).model.points,Environment(i).faceNormals,'1');
+    % First assign to true, so it enters the checking loop for this
+    % iteration of qMatrix
+    colResult = true;
+    % Loop here
+    while colResult == true
+        for i = 1:size(Environment,2)
+            colResult = IsCollision(Robot_Arm.model,qMatrix,Environment(i).model.faces,...
+                Environment(i).model.points,Environment(i).faceNormals,'1');
+            % Return as soon as a collision is detected
+            if colResult == true
+                return
+            end
+        end
     end
     
-    if colResult == true
-        disp('Collision detected');
-    else
-        Robot_Arm.model.animate(q);
-        if isempty(Objects) == 0
-            newBase = Robot_Arm.model.fkine(q);
-        end
-        for i = 1:size(Objects,2)
-            Objects(i).model.base = newBase * trotx(Objects(i).rot);
-            Objects(i).model.animate(0);
-        end
-        pause(0.01);
+    Robot_Arm.model.animate(q);
+    if isempty(Objects) == 0
+        newBase = Robot_Arm.model.fkine(q);
     end
+    for i = 1:size(Objects,2)
+        Objects(i).model.base = newBase * trotx(Objects(i).rot);
+        Objects(i).model.animate(0);
+    end
+    pause(0.01);
 end
 
 end
--- setup.m
+++ setup.m
@@ -10,8 +10,12 @@
 % Collision checking
 %   Code obtained from tutorial 5, and modified for use in the assignment
 %
-% D6 Model
-%   
+% N6 Model
+%   Model was taken from the Epson files.
+%   https://epson.com/For-Work/Robots/6-Axis/Flexion-N6-Compact-6-Axis-Robots---1000mm/p/RN6-A10SS73SS
+%
+% Rick Sanchez Model
+%   https://www.thingiverse.com/thing:2134321
 
 function [] = setup()
 %UNTITLED Summary of this function goes here
@@ -25,7 +29,7 @@ clc
 % Mac run command
 % run /Users/jonathanwilde/git/Robotics/rvctools/startup_rvc.m
 % Windows run command
-run C:\Git\Robotics\rvctools\startup_rvc.m
+% run C:\Git\Robotics\rvctools\startup_rvc.m
 
 %%
 hold on
@@ -36,14 +40,18 @@ workspace = [-3 3 -5 5 0 5]
 
 % setting up objects
 van = Objects('Van', '1', workspace, transl(0,0,0), 0);
-wildT = Objects('WildTurkey','2',workspace, transl(1,-1.85,1.87), -pi/2);
-smirn = Objects('Smirnoff', '3', workspace, transl(1,-1.65,1.87), -pi/2);
-glass = Objects('Glass', '4', workspace, transl(-1.3,-1,1.55), -pi/2);
-eStop1 = Objects('E-Stop', '5', workspace, transl(-1.15,1,1.4), pi/2);
+wildT = Objects('WildTurkey','2',workspace, transl(0.75,-1.85,1.87), -pi/2);
+smirn = Objects('Smirnoff', '3', workspace, transl(0.75,-1.65,1.87), -pi/2);
+glass = Objects('Glass', '4', workspace, transl(-1.1,-1,1.55), -pi/2);
+eStop1 = Objects('E-Stop', '5', workspace, transl(-1,1,1.4), pi/2);
+rick = Objects('Rick', '6', workspace, transl(-3,2,0), 0);
 
 % setting up  models
 N6_1 = D6Model('N6_1',workspace, transl(0,-0.5,0.650));
 N6_2 = D6Model('N6_2',workspace, transl(0,-2,0.600));
+
+% Adjust view
+view(300,20)
 
 % N6_1.model.teach();
 q = deg2rad([90,90,90,0,0,0])
