15be9f6431c7542f026f85aa515e945512123099
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-06-06 18:34:27 +1000

    playing with robot 2

--- onTheRocks.m
+++ onTheRocks.m
@@ -6,13 +6,13 @@ robots(1)
 
 pourPose = transl(-0.5,-1.8,1.5);
 
-MoveWObjects(robots(1), objects(1).getPose(), [], []);
-MoveWObjects(robots(1), pourPose * troty(pi/2), [objects(1)], []);
+MoveWObjects(robots(2), objects(1).getPose(), [], []);
+MoveWObjects(robots(2), pourPose * troty(pi/2), [objects(1)], []);
 
-[qMatrix, steps] = movementPour(robots(1), [], 2, objects,[], 50)
-MoveQMatrix(robots(1), qMatrix, objects(1), [], 1);
-[qMatrix, steps] = movementShake(robots(1), pourPose * transl(0,0,0.1), 5)
-MoveQMatrix(robots(1), qMatrix, objects(1), [], 50);
+[qMatrix, steps] = movementPour(robots(2), [], 2, objects,[], 50)
+MoveQMatrix(robots(2), qMatrix, objects(1), [], 1);
+[qMatrix, steps] = movementShake(robots(2), pourPose * transl(0,0,0.1), 5)
+MoveQMatrix(robots(2), qMatrix, objects(1), [], 50);
 
 end
 
