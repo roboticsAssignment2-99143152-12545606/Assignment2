7d4432a61f3f0f12dc2e85c7b9af5bc837aeabf1
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-06-06 17:07:47 +1000

    adding environment to moving functions

--- movementPour.m
+++ movementPour.m
@@ -1,4 +1,4 @@
-function [] = movementPour(Robot, GoalPose, Time, Objects, stepSize)
+function [] = movementPour(Robot, GoalPose, Time, Objects, environment,stepSize)
 %movementPour this function will simulate shaking for the robot
 %   code mainly used from lab9
 p1 = Robot.getPose;
@@ -6,9 +6,9 @@ iterations = 1
 % movementCircle(Robot, GoalPose, Time, Objects)
 motions = 0;
 for i=1:iterations
-    movementCircle(Robot, p1, Time/(iterations+motions), Objects, 3, 0.3, stepSize);
+    movementCircle(Robot, p1, Time/(iterations+motions), Objects, environment,3, 0.3, stepSize);
     %     movementCircle(Robot, p1, Time/iterations, Objects, -3, 0.3);
-    movementLine(Robot, transl(p1(1,4),p1(2,4)+0.2,p1(3,4)+0.2), Time/(iterations+motions), Objects, stepSize);
+    movementLine(Robot, transl(p1(1,4),p1(2,4)+0.2,p1(3,4)+0.2), Time/(iterations+motions), Objects, environment, stepSize);
 end
 
 end
