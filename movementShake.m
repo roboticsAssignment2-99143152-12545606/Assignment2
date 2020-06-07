ddb99d25b9379689d54fd4ccdd40e4de7ebbacc0
Jonathan <43630244+wildejono@users.noreply.github.com>
2020-06-07 11:00:38 +1000

    Revert "Merge branch 'master' into Demo"
    This reverts commit 306df0017d13c7eb4fe4b3bd93de1f9c0d25198f, reversing
    changes made to c03b5f3d52c2d6fc739da64014a82c55feb71b48.

--- movementShake.m
+++ movementShake.m
@@ -5,9 +5,7 @@ p1 = GoalPose;
 p2 = Robot.getPose;
 
 for i=0:4
-
-    [q, s] = movementLine(Robot(1), Robot(1).model.getpos,transl(p1(1,4),p1(2,4),p1(3,4)), Time);
-
+    [q, s] = movementLine(Robot, Robot.model.getpos,transl(p1(1,4),p1(2,4),p1(3,4)), Time);
     if ~exist('qMatrix','var')
         qMatrix = q;        
     else
@@ -18,9 +16,7 @@ for i=0:4
     else
         steps = steps + s;
     end  
-    
-    [q, s] = movementLine(Robot(1), qMatrix(end,:),transl(p2(1,4),p2(2,4),p2(3,4)), Time);
-
+    [q, s] = movementLine(Robot, qMatrix(end,:),transl(p2(1,4),p2(2,4),p2(3,4)), Time);
     qMatrix = [qMatrix;q];
     steps = steps + s;
 end
