1b35f675160efefce457bf66e592756684cf8268
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2020-06-08 15:27:51 +1000

    outputs from function

--- setup.m
+++ setup.m
@@ -17,7 +17,7 @@
 % Rick Sanchez Model
 %   https://www.thingiverse.com/thing:2134321
 
-function [] = setup()
+function [RobotArms,Objects,environmentObjects] = setup()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -53,6 +53,9 @@ workspace = [-3 3 -5 5 0 5];
 van = Objects('Van', '1', workspace, transl(0,0,0), 0);
 eStop1 = Objects('E-Stop', '5', workspace, transl(-1,1,1.4), pi/2);
 rick = Objects('Rick', '6', workspace, transl(-2,0,0), 0);
+
+environmentObjects = [van, eStop1, rick];
+
 %%  setting up objects
 wildT = Objects('WildTurkey','2',workspace, transl(0.5,-2.25,1.75), pi/2);
 smirn = Objects('Smirnoff', '3', workspace, transl(0.5,-1.65,1.75), -pi/2);
@@ -64,12 +67,16 @@ shaker = Objects('ShakerAssy', '6', workspace, transl(-0.5,-1.6,1.45), -pi/2);
 % Enable this shaker top for collision testing
 shakerTop = Objects('Shaker', '7', workspace, transl(-0.5,-2,1.55), -pi/2);
 
+Objects = [wildT,smirn,glass,spoonGlass,spoon,shaker,shakerTop];
+
 % Adjust view
 view(300,20);
 
 % setting up  models
 N6_1 = D6Model('N6_1',workspace, transl(-0.15,-1.6,0.605 + 0.4));
 N6_2 = D6Model('N6_2',workspace, transl(-0.15,-2.4,0.605 + 0.4));
+
+RobotArms = [N6_2, N6_1];
 
 % N6_1.model.teach();
 q = deg2rad([90,90,90,0,0,0]);
@@ -82,6 +89,7 @@ N6_2.model.animate(q);
 % movementPour(N6_1, [], 10, [bottle], 50)
 
 % onTheRocks([N6_2, N6_1],[wildT, glass],van);
+shakenNotstired([N6_2, N6_1],[smirn, shaker, shakerTop,glass],[van, eStop1, rick])
 
 'done'
 
